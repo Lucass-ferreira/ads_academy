cee15f323302b0297dd713a8cc38ff0b3fcb03eb
Lucas Ferreira <lucasferreiragxhxhs@gmail.com>
2025-04-07 16:14:04 -0300

    correção codigo

--- 2025_I/algoritimo_e_logica/modulo_3/exemplos/comando_if.c
+++ 2025_I/algoritimo_e_logica/modulo_3/exemplos/comando_if.c
@@ -3,17 +3,18 @@ int main(){
 
   float nota1, nota2, media;
 
-  printf("\n Informe as duas notas obtidas: ");
-  scanf("%f %f", &nota1, &nota2);
+  printf("\n Informe a primeira nota: ");
+  scanf("%f", &nota1);
+
+  printf("\n Informe a segunda nota: ");
+  scanf("%f", &nota2);
 
   media = (nota1 + nota2) / 2;
 
-  if ( media >= 7.0 ){
-    printf("Aluno Aprovado");
-  }  
-  else {
-    printf ( "Aluno Reprovado");
-  } 
+  if ( media >= 7.0 )
+    printf("\n Aluno Aprovado!");
+  else
+    printf ("\n Aluno Reprovado!");
 
   return 0;
 }
