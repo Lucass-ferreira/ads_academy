a71ded7ee453d55d5c5ed1e25ba1c54fdb785dc1
Lucas Ferreira <150602557+Lucass-ferreira@users.noreply.github.com>
2025-08-07 07:53:31 -0300

    funcao por valor

---
+++ 2025_I/algoritimo_e_logica/modulo_4/exemplos/funcao.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int sqr(int entrada)
+{
+  entrada = entrada * entrada;
+  return entrada;
+}
+
+int main(void)
+{
+  int t = 4;
+  printf("\n exemplo de funcao por VALOR");
+  printf("\n ----------------------------");
+  printf("\n %d %d", sqr(t), t);
+  return 0;
+}
