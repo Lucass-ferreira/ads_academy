9f269e0b6f0294ef8a92074a57900194615d5ae5
Lucas Ferreira <150602557+Lucass-ferreira@users.noreply.github.com>
2025-08-07 08:11:42 -0300

    reorganizando

---
+++ 2025_I/algoritimo_e_logica/modulo_4/exemplos/operadores_ponteiro.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+	int soma = 20 + 30;
+	int * somaParcial = &soma;
+	printf("\n Teste de utilizacao de uso de Ponteiros");
+	printf("\n *---------------------------------------*");
+	printf("\n Valor da variavel 'soma'= %d", soma);
+	printf("\n valor da variavel *SomaParcial = %d", * somaParcial);
+	return 0;
+}
